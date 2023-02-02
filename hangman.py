5b8583373b69363f047ebd013a83cffdacebc38c
Zynkah <ffgurl07@yahoo.com>
2023-02-02 10:57:44 -0800

    docstrings added

--- hangman.py
+++ hangman.py
@@ -18,9 +18,13 @@ def hangman():
     letter_board = ["__"] * len(word)
     win = False
     print('Welcome to Hangman')
+    
+    #user input for the each character of the possible word
     while wrong_guesses < len(stages) - 1:
         print('\n')
         guess = input("Guess a letter ").lower()
+        
+        #if letter is correctly guessed use $ in place of the correct letter otherwise increment by 1 the incorrect guess
         if guess in remaining_letters:
             character_index = remaining_letters.index(guess)
             letter_board[character_index] = guess
@@ -29,11 +33,15 @@ def hangman():
             wrong_guesses += 1
         print((' '.join(letter_board)))
         print('\n'.join(stages[0: wrong_guesses + 1]))
+        
+        #code for winning
         if '__' not in letter_board:
             print('You win! The word was:')
             print(' '.join(letter_board))
             win = True
             break
+    
+    #code for losing    
     if not win:
         print('\n'.join(stages[0: wrong_guesses]))
         print('You lose! The words was {}'.format(word))
